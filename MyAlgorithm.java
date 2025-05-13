b7f1245f6db9817c3fed2e54cc7b4fdc239d63f4
Wisit Suwannao <wisit.p.2005@gmail.com>
2025-05-13 20:38:42 +0700

    latest update
    Add some function (bfsDistance, height, balanceFactor)

--- MyAlgorithm.java
+++ MyAlgorithm.java
@@ -8,97 +8,85 @@ public class MyAlgorithm extends Algorithm {
         super();
     }
 
-    public BST cloneTree(BST tree) {
-        BST newTree = new BST();
-        List<Integer> Order = preorder(tree);
-        for (int i : Order) {
-            newTree.insertNode(i);
-        }
-        return newTree;
-    }
-
-    public BST insertDuplicateTree(BST tree, int node) {
-        BST newTree = cloneTree(tree);
-        newTree.insertNode(node);
-        return newTree;
-    }
-
-    public int findBalance(BST tree, int Node) {
-        BinNode targetNode = findNode(tree.root, Node);
-        if (targetNode == null)
-            return -1;
-        int leftHeight = findHeight(targetNode.leftChild);
-        int rightHeight = findHeight(targetNode.rightChild);
-        return leftHeight - rightHeight;
-    }
-
-    public int findHeight(BST tree, int Node) {
-        BinNode targetNode = findNode(tree.root, Node);
-        if (targetNode == null)
-            return -1;
-        int leftHeight = findHeight(targetNode.leftChild);
-        int rightHeight = findHeight(targetNode.rightChild);
-        return Math.max(leftHeight, rightHeight) + 1;
-    }
-
-    private int findHeight(BinNode curNode) {
-        if (curNode == null)
-            return 0;
-        int leftHeight = findHeight(curNode.leftChild);
-        int rightHeight = findHeight(curNode.rightChild);
-        return Math.max(leftHeight, rightHeight) + 1;
-    }
-
-    private BinNode findNode(BinNode curNode, int Node) {
-        if (curNode == null)
+    public BinNode duplicateTree(BinNode node) {
+        if (node == null) {
             return null;
-        if (curNode.node == Node)
-            return curNode;
-        if (Node < curNode.node)
-            return findNode(curNode.leftChild, Node);
-        return findNode(curNode.rightChild, Node);
+        }
+        BinNode newNode = new BinNode(node.node);
+        newNode.leftChild = duplicateTree(node.leftChild);
+        newNode.rightChild = duplicateTree(node.rightChild);
+        return newNode;
+    }
+
+    public int height(BinNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return Math.max(height(node.leftChild), height(node.rightChild)) + 1;
+    }
+
+    public int getBalanceFactor(BinNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return height(node.leftChild) - height(node.rightChild);
+    }
+
+    public BinNode searchNode(BinNode root, int key) {
+        if (root == null || root.node == key) {
+            return root;
+        }
+
+        if (key < root.node) {
+            return searchNode(root.leftChild, key);
+        } else {
+            return searchNode(root.rightChild, key);
+        }
     }
 
     public List<Integer> preorder(BST tree) {
         List<Integer> result = new ArrayList<>();
-        preorderUtil(tree.root, result);
+        preorderTraversal(tree.root, result);
         return result;
     }
 
-    public void preorderUtil(BinNode curNode, List<Integer> result) {
-        if (curNode != null) {
-            result.add(curNode.node);
-            preorderUtil(curNode.leftChild, result);
-            preorderUtil(curNode.rightChild, result);
+    public void preorderTraversal(BinNode curNode, List<Integer> result) {
+        if (curNode == null) {
+            return;
         }
+        result.add(curNode.node);
+        preorderTraversal(curNode.leftChild, result);
+        preorderTraversal(curNode.rightChild, result);
     }
 
     public List<Integer> inorder(BST tree) {
         List<Integer> result = new ArrayList<>();
-        inorderUtil(tree.root, result);
+        inorderTraversal(tree.root, result);
         return result;
     }
 
-    public void inorderUtil(BinNode curNode, List<Integer> result) {
-        if (curNode != null) {
-            inorderUtil(curNode.leftChild, result);
-            result.add(curNode.node);
-            inorderUtil(curNode.rightChild, result);
+    public void inorderTraversal(BinNode curNode, List<Integer> result) {
+        if (curNode == null) {
+            return;
         }
+        inorderTraversal(curNode.leftChild, result);
+        result.add(curNode.node);
+        inorderTraversal(curNode.rightChild, result);
     }
 
     public List<Integer> postorder(BST tree) {
         List<Integer> result = new ArrayList<>();
-        postorderUtil(tree.root, result);
+        postorderTraversal(tree.root, result);
         return result;
     }
 
-    public void postorderUtil(BinNode curNode, List<Integer> result) {
-        if (curNode != null) {
-            postorderUtil(curNode.leftChild, result);
-            postorderUtil(curNode.rightChild, result);
-            result.add(curNode.node);
+    public void postorderTraversal(BinNode curNode, List<Integer> result) {
+        if (curNode == null) {
+            return;
         }
+        postorderTraversal(curNode.leftChild, result);
+        postorderTraversal(curNode.rightChild, result);
+        result.add(curNode.node);
     }
 
     public List<Integer> dfs(GraphM graph, int startNode) {
@@ -130,120 +118,121 @@ public class MyAlgorithm extends Algorithm {
             int current = queue.poll();
             result.add(current);
             for (Pair<Integer, Integer> neighbor : graph.adjacencyList.getOrDefault(current, new ArrayList<>())) {
+                int neighborNode = neighbor.first;
                 if (!isVisited[neighbor.first]) {
-                    queue.add(neighbor.first);
                     isVisited[neighbor.first] = true;
+                    queue.add(neighbor.first);
                 }
             }
         }
         return result;
     }
 
+    public int bfsDistance(GraphL graph, int startNode, int destNode) {
+        List<Integer> result = new ArrayList<>();
+        boolean[] isVisited = new boolean[graph.numVertices];
+        Queue<Integer> queue = new LinkedList<>();
+        int[] dist = new int[graph.numVertices];
+
+        Arrays.fill(dist, -1);
+        queue.add(startNode);
+        isVisited[startNode] = true;
+        dist[startNode] = 0;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            result.add(current);
+            for (Pair<Integer, Integer> neighbor : graph.adjacencyList.getOrDefault(current, new ArrayList<>())) {
+                int neighborNode = neighbor.first;
+                if (!isVisited[neighbor.first]) {
+                    isVisited[neighbor.first] = true;
+                    dist[neighborNode] = dist[current] + 1;
+                    queue.add(neighbor.first);
+                }
+            }
+        }
+        return -1;
+    }
+
     public int floyd(GraphM graph, int startNode, int destNode) {
         int[][] dist = new int[graph.numVertices][graph.numVertices];
         for (int i = 0; i < graph.numVertices; i++) {
             for (int j = 0; j < graph.numVertices; j++) {
-                dist[i][j] = graph.matrix[i][j];
+                if (i == j) {
+                    dist[i][j] = 0;
+                } else if (dist[i][j] != 0) {
+                    dist[i][j] = graph.matrix[i][j];
+                } else {
+                    dist[i][j] = Integer.MAX_VALUE;
+                }
             }
         }
-        // หรือใช้ int[][] dist = graph.matrix.clone(); แทนการลูปทั้งหมดก็ได้
 
         for (int k = 0; k < graph.numVertices; k++) {
             for (int i = 0; i < graph.numVertices; i++) {
+                if (dist[i][k] == Integer.MAX_VALUE) {
+                    continue;
+                }
                 for (int j = 0; j < graph.numVertices; j++) {
-                    if (dist[i][k] != 0 && dist[k][j] != 0 && i != j) {
-                        if (dist[i][j] == 0) {
-                            dist[i][j] = dist[i][k] + dist[k][j];
-                        } else {
-                            dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-                        }
+                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                     }
                 }
             }
         }
-        return dist[startNode][destNode];
-    }
-
-    public int[][] floydMatrix(GraphM graph) {
-        int[][] dist = new int[graph.numVertices][graph.numVertices];
-        for (int i = 0; i < graph.numVertices; i++) {
-            for (int j = 0; j < graph.numVertices; j++) {
-                dist[i][j] = graph.matrix[i][j];
-            }
-        }
-        // หรือใช้ int[][] dist = graph.matrix.clone(); แทนการลูปทั้งหมดก็ได้
-
-        for (int k = 0; k < graph.numVertices; k++) {
-            for (int i = 0; i < graph.numVertices; i++) {
-                for (int j = 0; j < graph.numVertices; j++) {
-                    if (dist[i][k] != 0 && dist[k][j] != 0 && i != j) {
-                        if (dist[i][j] == 0) {
-                            dist[i][j] = dist[i][k] + dist[k][j];
-                        } else {
-                            dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-                        }
-                    }
-                }
-            }
-        }
-        return dist;
+        return dist[startNode][destNode] == Integer.MAX_VALUE ? -1 : dist[startNode][destNode];
     }
 
     public boolean warshall(GraphM graph, int startNode, int destNode) {
-        int[][] dist = new int[graph.numVertices][graph.numVertices];
-        // หรือใช้ int[][] dist = graph.matrix.clone(); ก็ได้
-        for (int k = 0; k < graph.numVertices; k++) {
-            for (int i = 0; i < graph.numVertices; i++) {
-                for (int j = 0; j < graph.numVertices; j++) {
-                    if (dist[i][k] != 0 && dist[k][j] != 0) {
-                        dist[i][j] = 1;
-                    }
+        boolean[][] reach = new boolean[graph.numVertices][graph.numVertices];
+        for (int i = 0; i < graph.numVertices; i++) {
+            for (int j = 0; j < graph.numVertices; j++) {
+                if (i == j || graph.matrix[i][j] != 0) {
+                    reach[i][j] = true;
                 }
             }
         }
-        return (dist[startNode][destNode] == 1);
-    }
 
-    public int[][] warshallMatrix(GraphM graph) {
-        int[][] dist = new int[graph.numVertices][graph.numVertices];
-        // หรือใช้ int[][] dist = graph.matrix.clone(); ก็ได้
         for (int k = 0; k < graph.numVertices; k++) {
             for (int i = 0; i < graph.numVertices; i++) {
                 for (int j = 0; j < graph.numVertices; j++) {
-                    if (dist[i][k] == 1 && dist[k][j] == 1) {
-                        dist[i][j] = 1;
+                    if (reach[i][k] && reach[k][j]) {
+                        reach[i][j] = true;
                     }
                 }
             }
         }
-        return dist;
+        return reach[startNode][destNode];
     }
 
     public int dijkstra(GraphL graph, int startNode, int destNode) {
-        int[] result = new int[graph.numVertices];
-        boolean[] isVisited = new boolean[graph.numVertices];
-        Arrays.fill(result, Integer.MAX_VALUE);
-        result[startNode] = 0;
-        for (int i = 0; i < graph.numVertices; i++) {
-            int curNode = -1;
-            int minDist = Integer.MAX_VALUE;
-            for (int j = 0; j < graph.numVertices; j++) {
-                if (!isVisited[j] && result[j] < minDist) {
-                    minDist = result[j];
-                    curNode = j;
-                }
-            }
-            if (curNode == -1) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparing(pair -> pair[1]));
+        int[] dist = new int[graph.numVertices + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[startNode] = 0;
+
+        pq.add(new int[] { startNode, 0 });
+
+        while (!pq.isEmpty()) {
+            int[] current = pq.poll();
+            int curNode = current[0];
+
+            if (curNode == destNode) {
                 break;
             }
 
-            isVisited[curNode] = true;
-            for (Pair<Integer, Integer> neighbor : graph.adjacencyList.get(curNode)) {
-                if (!isVisited[neighbor.first] && result[curNode] + neighbor.second < result[neighbor.first]) {
-                    result[neighbor.first] = result[curNode] + neighbor.second;
+            if (graph.adjacencyList.containsKey(curNode)) {
+                for (Pair<Integer, Integer> neighbor : graph.adjacencyList.get(curNode)) {
+                    int neighborNode = neighbor.first;
+                    int weight = neighbor.second;
+
+                    if (dist[curNode] + weight < dist[neighborNode]) {
+                        dist[neighborNode] = dist[curNode] + weight;
+                        pq.add(new int[] { neighborNode, dist[neighborNode] });
+                    }
                 }
             }
         }
-        return result[destNode];
+        return dist[destNode] == Integer.MAX_VALUE ? -1 : dist[destNode];
     }
 }
